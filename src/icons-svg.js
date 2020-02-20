5fc9d985d4a83e4147087879c78e01873f86ae02
Rid <rid@cylo.io>
2020-02-20 10:59:17 +0000

    💄 Updated folder open
    Signed-off-by: Rid <rid@cylo.io>

--- src/icons-svg.js
+++ src/icons-svg.js
@@ -9,15 +9,15 @@ export default {
   createNewFolder: `<svg  xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48" focusable="false"><path d="M40 12H24l-4-4H8c-2.21 0-3.98 1.79-3.98 4L4 36c0 2.21 1.79 4 4 4h32c2.21 0 4-1.79 4-4V16c0-2.21-1.79-4-4-4zm-2 16h-6v6h-4v-6h-6v-4h6v-6h4v6h6v4z"/></svg>`,
   delete: `<svg  xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48" focusable="false"><path d="M12 38c0 2.21 1.79 4 4 4h16c2.21 0 4-1.79 4-4V14H12v24zM38 8h-7l-2-2H19l-2 2h-7v4h28V8z"/></svg>`,
   rename: `<svg class="a-s-fa-Ha-pa" x="0px" y="0px" width="24px" height="24px" viewBox="0 0 24 24" focusable="false" fill="rgba(0, 0, 0, 0.72)"><path d="M0 0h24v24H0z" fill="none"></path><path d="M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zM6 17v-2.47l7.88-7.88c.2-.2.51-.2.71 0l1.77 1.77c.2.2.2.51 0 .71L8.47 17H6zm12 0h-7.5l2-2H18v2z"></path></svg>`,
-  open: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M20 6h-8l-2-2H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2zm0 12H4V8h16v10z"/></svg>`,
-  folder: `<svg width="46px" height="35px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 46 35" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-      <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-          <path d="M40,9 L25.1304348,9 L20.9565217,9 L8.43478261,9 C6.12869565,9 4.28173913,10.8539286 4.28173913,13.1428571 L0.000616642031,27.2600296 L0.02,4 C0.02,1.79 1.79,0 4,0 L16,0 L20,4 L36,4 C38.21,4 40,5.79 40,8 L40,9 Z" id="Combined-Shape" fill="#000000" fill-rule="nonzero"></path>
-          <g id="folder" transform="translate(0.000000, 11.000000)" fill="#000000" fill-rule="nonzero">
-              <path d="M22,0 L10,0 C7.79,0 6.02,1.53428571 6.02,3.42857143 L0,20.5714286 C0,22.4657143 1.79,24 4,24 L36,24 C38.21,24 40,22.4657143 40,20.5714286 L46,3.42857143 C46,1.53428571 44.21,0 42,0 L26,0 L22,0 Z" id="Path"></path>
-          </g>
-      </g>
+  open: `<svg width="47px" height="36px" viewBox="0 0 47 36" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+    <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+        <path d="M40,9 L25.1304348,9 L20.9565217,9 L8.43478261,9 C6.12869565,9 4.28173913,10.8539286 4.28173913,13.1428571 L0.000616642031,27.2600296 L0.02,4 C0.02,1.79 1.79,0 4,0 L16,0 L20,4 L36,4 C38.21,4 40,5.79 40,8 L40,9 Z" id="Combined-Shape" fill="#000000" fill-rule="nonzero"></path>
+        <g id="folder" transform="translate(1.000000, 12.000000)" fill="#000000" fill-rule="nonzero">
+            <path d="M22,0 L10,0 C7.79,0 6.02,1.53428571 6.02,3.42857143 L0,20.5714286 C0,22.4657143 1.79,24 4,24 L36,24 C38.21,24 40,22.4657143 40,20.5714286 L46,3.42857143 C46,1.53428571 44.21,0 42,0 L26,0 L22,0 Z" id="Path"></path>
+        </g>
+    </g>
   </svg>`,
+  folder: `<svg  xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48" focusable="false"><path d="M20 8H8c-2.21 0-3.98 1.79-3.98 4L4 36c0 2.21 1.79 4 4 4h32c2.21 0 4-1.79 4-4V16c0-2.21-1.79-4-4-4H24l-4-4z"/></svg>`,
   volumeUp: `<svg  xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48" focusable="false"><path d="M6 18v12h8l10 10V8L14 18H6zm27 6c0-3.53-2.04-6.58-5-8.05v16.11c2.96-1.48 5-4.53 5-8.06zM28 6.46v4.13c5.78 1.72 10 7.07 10 13.41s-4.22 11.69-10 13.41v4.13c8.01-1.82 14-8.97 14-17.54S36.01 8.28 28 6.46z"/></svg>`,
   image: `<svg  xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48" focusable="false"><path d="M42 38V10c0-2.21-1.79-4-4-4H10c-2.21 0-4 1.79-4 4v28c0 2.21 1.79 4 4 4h28c2.21 0 4-1.79 4-4zM17 27l5 6.01L29 24l9 12H10l7-9z"/></svg>`,
   ondemandVideo: `<svg  xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48" focusable="false"><path d="M42 6H6c-2.21 0-4 1.79-4 4v24c0 2.21 1.79 4 4 4h10v4h16v-4h10c2.21 0 3.98-1.79 3.98-4L46 10c0-2.21-1.79-4-4-4zm0 28H6V10h36v24zM32 22l-14 8V14z"/></svg>`,
